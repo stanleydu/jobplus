fef82eb0f3c687bd4aa2ed7d082d1d4fe0eb72c5
pdu <1090416@qq.com>
2020-08-04 16:38:33 +0800

    base file

---
+++ jobplus/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# _*_ coding:utf-8 _*_
+"""
+@version: v1.0
+@author = pdu
+@license: pdu Licence 
+@contact: 1090416@qq.com
+@project = jobplus
+@file: __init__.py
+@create_time = 2020/8/44:22 下午
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
